ab7cd842e64f108fc4e76c670ce0a7dec41f373c
Nikita Moskaliov <nikwit10@gmail.com>
2024-11-27 10:04:48 +0300

    Task 3 test templates

---
+++ tests/task3.cpp
@@ -0,0 +1,31 @@
+#include "fuzztest/fuzztest.h"
+#include <gtest/gtest.h>
+
+namespace LR6 {
+namespace testing {
+using ::fuzztest::internal_no_adl::InRegexp;
+
+
+auto PushChars(std::string s, size_t count, std::string chr) {
+  using namespace fuzztest;
+  return 
+}
+
+
+auto SymmetricalString (auto max_size) {
+  auto size = ::fuzztest::InRange(0, max_size/2);
+  return ::fuzztest::FlatMap([] (size_t x) {
+      return ::fuzztest::VectorOf(InRegexp("\\p{Cyrillic}")).WithSize(x);
+    },
+    size
+  );
+}
+
+void PrintSth(const std::string &s) {
+  std::cout << s << '\n';
+}
+
+FUZZ_TEST(UNTEST, PrintSth).WithDomains(SymmetricalString(40));
+
+}  // namespace testing
+}  // namespace LR6
